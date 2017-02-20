168d638443565c40ee559635f526795082b9af6f
PreeTong <mukuro281@gmail.com>
2017-02-20 15:52:23 +0700

    Update Test.php

--- Test.php
+++ Test.php
@@ -1,15 +1,9 @@
 <?php
 $access_token = 'Lc9YWVYk4V568kkxsRx39DcF9TQJzs+h1tgLO4Bp3W6Srm1ZlXm8XuTGfPXUhsEm/pJjk3HM4CAM0uKyXHXjEZtFa6jW6nKBbrM5ScR76OzBGv2aVsCzCdAZNX41i+scHHD4wa77RISbPAOPwoIDvAdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/events';
+$channelSecret = 'e3d5f27d85d1b1afb3f8dbe589cd1ce5'; // Channel secret string
+$httpRequestBody = 'group'; // Request body string
+$hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
+$signature = base64_encode($hash);
+// Compare X-Line-Signature request header string and the signature
 
-$headers = array('Authorization: Bearer ' . $access_token);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result;
